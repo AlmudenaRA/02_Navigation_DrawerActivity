c9446bf7ffa186593fda2075a2ab16203ff75360
AlmudenaRA <almudena.ruedas@gmail.com>
2020-04-11 22:07:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a02_navigation_draweractivity/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.a02_navigation_draweractivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_averias, R.id.nav_talleres, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_logout)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        //Personalización del contenido del NavigationView header
+        ImageView ivAvatar = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.imageViewAvatar);
+        ivAvatar.setImageResource(R.drawable.ic_user);
+
+        TextView nombre = (TextView) navigationView.getHeaderView(0).findViewById(R.id.textViewNombre);
+        TextView email = (TextView) navigationView.getHeaderView(0).findViewById(R.id.textViewEmail);
+
+        nombre.setText("Almudena");
+        email.setText("almudena.ruedas@gmail.com");
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                boolean fragmentTransaction = false;
+                Fragment f = null;
+
+                if (id == R.id.nav_averias){
+                    f = new ListadoAveriasFragment();
+                }else if (id == R.id.nav_talleres){
+                    f = new ListaTalleresFragment();
+                }else if (id == R.id.nav_share){
+
+                }else if (id == R.id.nav_logout){
+
+                }
+
+                if(f != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.contenedor, f)
+                            .commit();
+
+                    menuItem.setChecked(true);
+                    getSupportActionBar().setTitle(menuItem.getTitle());
+                }
+
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+
+}
+
+
---
+++ app/src/main/java/com/example/a02_navigation_draweractivity/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,20 @@
+package com.example.a02_navigation_draweractivity.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
